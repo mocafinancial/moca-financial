8bfd2c380cfccbba7dc8fa180ecb1263dcb13e0a
Zane Rockenbaugh <zane@liquid-labs.com>
2019-12-12 19:36:43 -0600

    upated package registry

--- settings.sh
+++ settings.sh
@@ -4,6 +4,6 @@ ORG_LEGAL_NAME='Moca Financial Inc'
 ORG_ADDRESS='430 Old Firetower Rd
 Bastrop, TX 78602'
 ORG_NAICS='1'
-ORG_NPM_REGISTRY='https://npm.pkg.github.com/mocafinancial'
+ORG_NPM_REGISTRY='https://npm.pkg.github.com/'
 ORG_NPM_SCOPE='mocafinancial'
 ORG_NICK_NAME='moca'
